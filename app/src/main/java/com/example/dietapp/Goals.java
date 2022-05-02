1d07dcc9186caceb4bb28e6c956013e0ae922c3b
Emmanuel-Junior <81275130+Emmanuel-Junior@users.noreply.github.com>
2022-05-02 10:26:09 +0100

    nothing

--- app/src/main/java/com/example/dietapp/Goals.java
+++ app/src/main/java/com/example/dietapp/Goals.java
@@ -9,8 +9,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Goals extends AppCompatActivity {
+    String Lweight;
+    String Mweight;
+    String Gweight;
+
+    Button losew;
+    Button maintainw;
+    Button gainw;
 
     private Button button;
     int i;
@@ -19,12 +27,21 @@ public class Goals extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
 
+        losew = (Button) findViewById(R.id.loseweight);
+        maintainw = (Button) findViewById(R.id.maintainweight);
+        gainw = (Button) findViewById(R.id.gainweight);
+        selectedButton();
         button = (Button) findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opennextPage();
-            }
+
+                    Lweight = losew.getText().toString();
+                    Mweight = maintainw.getText().toString();
+                    Gweight = gainw.getText().toString();
+                    opennextPage();
+                }
+
         });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -46,15 +63,67 @@ public class Goals extends AppCompatActivity {
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
-    public void onOutlinedbtnclicked(View view){
 
-        //change button color
-        Button button = findViewById(view.getId());
-        GradientDrawable gradientDrawable = new GradientDrawable();
-        gradientDrawable.setStroke(2,getResources().getColor(R.color.green_2));
-        button.setBackground(gradientDrawable);
-        //change text color
-        button.setTextColor(getResources().getColor(R.color.green_2));
+    public void selectedButton(){
+        losew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //change button color
+
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.setStroke(2,getResources().getColor(R.color.green_2));
+                losew.setBackground(gradientDrawable);
+
+                GradientDrawable gradientDrawable1 = new GradientDrawable();
+                gradientDrawable1.setStroke(2,getResources().getColor(R.color.black));
+                maintainw.setBackground(gradientDrawable1);
+                gainw.setBackground(gradientDrawable1);
+                //change text color
+                losew.setTextColor(getResources().getColor(R.color.green_2));
+                maintainw.setTextColor(getResources().getColor(R.color.black));
+                gainw.setTextColor(getResources().getColor(R.color.black));
+
+            }
+        });
+        maintainw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //change button color
+
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.setStroke(2,getResources().getColor(R.color.green_2));
+                maintainw.setBackground(gradientDrawable);
+
+                GradientDrawable gradientDrawable1 = new GradientDrawable();
+                gradientDrawable1.setStroke(2,getResources().getColor(R.color.black));
+                losew.setBackground(gradientDrawable1);
+                gainw.setBackground(gradientDrawable1);
+                //change text color
+                maintainw.setTextColor(getResources().getColor(R.color.green_2));
+                losew.setTextColor(getResources().getColor(R.color.black));
+                gainw.setTextColor(getResources().getColor(R.color.black));
+
+            }
+        });
+        gainw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //change button color
+
+                GradientDrawable gradientDrawable = new GradientDrawable();
+                gradientDrawable.setStroke(2,getResources().getColor(R.color.green_2));
+                gainw.setBackground(gradientDrawable);
+                GradientDrawable gradientDrawable1 = new GradientDrawable();
+                gradientDrawable1.setStroke(2,getResources().getColor(R.color.black));
+                losew.setBackground(gradientDrawable1);
+                maintainw.setBackground(gradientDrawable1);
+                //change text color
+                gainw.setTextColor(getResources().getColor(R.color.green_2));
+                losew.setTextColor(getResources().getColor(R.color.black));
+                maintainw.setTextColor(getResources().getColor(R.color.black));
+
+            }
+        });
 
     }
 }
