476db8470933acdfe4ec74d87a222a8cfff8a26a
Awasum Precious <97224559+wallang-A-M-P@users.noreply.github.com>
2022-05-16 10:34:22 +0100

    homepage work

--- app/src/main/java/com/example/dietapp/Diary.java
+++ app/src/main/java/com/example/dietapp/Diary.java
@@ -61,5 +61,8 @@ public class Diary extends AppCompatActivity {
                 return false;
             }
         });
+
+
+
     }
 }
--- app/src/main/java/com/example/dietapp/Home.java
+++ app/src/main/java/com/example/dietapp/Home.java
@@ -1,11 +1,16 @@
 package com.example.dietapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,6 +23,9 @@ public class Home extends AppCompatActivity {
 
         homenavbar();
     }
+
+
+    /*Navbar Manipulations*/
     public void homenavbar(){
         //Initialize and assign Variables
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -49,5 +57,6 @@ public class Home extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 }
