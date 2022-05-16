7ae2a2e7733d2ec5e48e923daf3c0072e3f67b6d
Emmanuel-Junior <81275130+Emmanuel-Junior@users.noreply.github.com>
2022-05-16 12:28:35 +0100

    changes

---
+++ app/src/main/java/com/example/dietapp/selectmeal.java
@@ -0,0 +1,14 @@
+package com.example.dietapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectmeal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectmeal);
+    }
+}
