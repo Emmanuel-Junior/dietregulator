33fd254ef9c4b599773e0606b21f32f50a3ed51a
Emmanuel-Junior <81275130+Emmanuel-Junior@users.noreply.github.com>
2022-06-10 05:54:53 +0100

    changes

--- app/src/main/java/com/example/dietapp/Splashscreen.java
+++ app/src/main/java/com/example/dietapp/Splashscreen.java
@@ -11,17 +11,40 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 public class Splashscreen extends AppCompatActivity {
     private static int SPLASH_SCREEN = 3500;
 //variables
     Animation topanim;
     ImageView image;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN= 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
+//        mUser = mAuth.getCurrentUser();
         animation();
         splashscreen();
     }
